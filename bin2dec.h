f1cac4927ce653772120c63cdb7efc1439e396b7
Ardeshir <46844576+ArdeshirV@users.noreply.github.com>
2019-01-29 00:21:53 +0330

    Update bin2dec.h

--- bin2dec.h
+++ bin2dec.h
@@ -9,18 +9,19 @@
 #define C_WARNING "\033[0;31m"/* Warning: Red                                 */
 #define C_NORMAL "\033[0m"    /* Normal Text: White                           */
 #define C_USER "\033[1;32m"   /* User Input: Bold Green                       */
-#define C_INPUT "\033[0;32m"   /* Reprinting User Input in Output: Green      */
+#define C_INPUT "\033[0;32m"  /* Reprinting User Input in Output: Green       */
 #define C_RESULT "\033[0;35m" /* Result and Output: Magenta                   */
 #define C_TITLE "\033[0;37m"  /* Title: Yellow                                */
 #define C_INFO "\033[1;37m"   /* Information of Title: Bold Yellow or Brown   */
+#define C_QUES "\033[0;36m"   /* Questions and Input Request: Light Blue      */
+#define C_BQUES "\033[1;36m"  /* Bold Questions: Bold Light Blue              */
 /*----------------------------------------------------------------------------*/
 typedef unsigned long ulong;
 typedef unsigned char boolean;
 /*----------------------------------------------------------------------------*/
-//ulong powers[128];
 ulong power2(ulong num);
-ulong Bin2Dec(char[], int);
-boolean IsValidBinary(char[], int);
+ulong Bin2Dec(const char[], const int);
+boolean IsValidBinary(const char arr[], const int len);
 void PrintTitle(char[], const char[], const char[],
                 const char[], boolean color);
 void PrintCopyright(char[], const char[], const char[],
